a1b0f1fb066ff743b3bdc12eb5f7464a9db1ec64
Ratchaneekorn S <i_hear_nie@hotmail.com>
2024-03-25 00:28:49 +0700

    feat: manage config

--- bank/main.go
+++ bank/main.go
@@ -4,19 +4,23 @@ import (
 	"bank/handler"
 	"bank/repository"
 	"bank/service"
+	"fmt"
+	"log"
 	"net/http"
+	"strings"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"github.com/jmoiron/sqlx"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	db, err := sqlx.Open("mysql", "root:12345678@tcp(localhost:3306)/banking")
-	if err != nil {
-		panic(err)
-	}
+	initTimeZone()
+	initConfig()
 
+	db := initDatabase()
 	customerRepositoryDB := repository.NewCustomerRepositoryDB(db)
 	customerRepositoryMock := repository.NewCustomerRepositoryMock() //ลองสลับไปใช้ data mockup ดูได้
 	_ = customerRepositoryMock
@@ -57,7 +61,51 @@ func main() {
 	// > curl localhost:8000/customers -i จะเห็น header ด้วย
 	router.HandleFunc("/customers/{customerID:[0-9]+}", customerHandler.GetCustomer).Methods(http.MethodGet)
 
-	http.ListenAndServe(":8000", router)
+	log.Printf("Banking service started at port %v", viper.GetInt("app.port"))
+	http.ListenAndServe(fmt.Sprintf(":%v", viper.GetInt("app.port")), router)
 
-	/////////// Service ///////////
+}
+
+func initConfig() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_")) //ใช้กรณี replace env ตอน start
+	//> APP_PORT=3001 go run .
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func initTimeZone() {
+	ict, err := time.LoadLocation("Asia/Bangkok")
+	if err != nil {
+		panic(err)
+	}
+
+	time.Local = ict
+}
+
+func initDatabase() *sqlx.DB {
+	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v",
+		viper.GetString("db.username"),
+		viper.GetString("db.password"),
+		viper.GetString("db.host"),
+		viper.GetInt("db.port"),
+		viper.GetString("db.database"),
+	)
+
+	db, err := sqlx.Open(viper.GetString("db.driver"), dsn)
+	if err != nil {
+		panic(err)
+	}
+
+	db.SetConnMaxLifetime(3 * time.Minute)
+	db.SetMaxOpenConns(10) // เปิดพร้อมกันได้กี่อัน
+	db.SetMaxIdleConns(10)
+
+	return db
 }
